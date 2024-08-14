04806608b86af48b3ed881ecb21eb08fe4d474d1
Harish.A <asianpirate27@gmail.com>
2024-08-14 10:16:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Front from './Components/Front'
+export default function App() {
+  return (
+    <>
+    <Front/>
+    </>
+  )
+}
---
+++ src/Components/Front.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Front() {
+  return (
+    <>
+    <div className='Nav'>
+    <div className='Nav1'>
+   <ul>
+    <li><a href="">ABOUT US</a></li>
+    <li><a href="">GALLERY</a></li>
+    <li><a href="">EVENTS</a></li>
+   </ul>
+    </div>
+    <div className='Nav2'>
+<ul>
+    <li><a href="">BLOG</a></li>
+    <li><a href="">EMPLOYMENT</a></li>
+    <li><a href="">CONTACT</a></li>
+</ul>
+    </div>
+    </div>
+    <div className='im'>
+        <img src="src\assets\Images\i6.jpg" alt="" />
+        <button className='btn1'>CALL  NOW</button>
+    </div>
+    </>
+  )
+}
