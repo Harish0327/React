3f224a69e4b52c46f2830be8302cf7bd5f5b98ac
Harish.A <asianpirate27@gmail.com>
2024-08-16 16:10:27 +0530

    first commit

--- src/Components/Front.jsx
+++ src/Components/Front.jsx
@@ -20,8 +20,37 @@ export default function Front() {
     </div>
     </div>
     <div className='im'>
-        <img src="src\assets\Images\i6.jpg" alt="" />
+        <img src="src\assets\Images\h5.jpg" alt="" />
+        <div className='tag'>
+        <h1>CAMEORN </h1>
+        <q> Turning moments into memories </q>
+        </div>
         <button className='btn1'>CALL  NOW</button>
+        
+    </div>
+    <div className='About'>
+    <h1>About us</h1>
+    <img src="src\assets\Images\i1.jpg" alt="" />
+   <p>We specialize in wedding photography, corporate, family and senior portraits, often traveling to your destination to capture the perfect moment in the perfect place. From the shores here in Lake Geneva to the beaches of Lake Michigan in Milwaukee and Chicago and beyond. We will be there with you every step of the way to guarantee your special moments are captured for all time. <br /><br />
+
+To us photography is about people being real and then letting us paint a picture of that moment to remember it forever. This is the story that matters most: real people, real stories, real moments. <br /><br />
+
+We are incredibly blessed to have a team of photographers who work with us on any given weekend. Please take a few minutes to get to know us. <br /><br />
+
+We would love to visit with you by phone or in person and talk about your wedding, event, portrait session or video project. Please reach out to us through the Contact Page. <br /><br />
+
+</p>
+
+    </div>
+    <h1 className='prev1'>Our Previous Works</h1>
+    <div className='prev'>
+
+<img src="src\assets\Images\1494890785798-evgenia-kostiaeva-10.jpeg" alt="" />
+<img src="src\assets\Images\Fun+ring+finger+photo.jpeg" alt="" />
+<img src="src\assets\Images\MG_3291(pp_w768_h512).jpg" alt="" />
+<img src="src\assets\Images\im2.jpg" alt="" />
+<img src="src\assets\Images\4543.jpg" alt="" />
+<img src="src\assets\Images\castaway_portland_wedding_photos_award_winning_wedding_photographers_josandtree-2.jpg" alt="" />
     </div>
     </>
   )
